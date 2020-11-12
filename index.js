604d8ec10b62c167cd78ad35faedaac0556b852d
ndom91 <yo@ndo.dev>
2020-11-12 16:07:01 +0100

    fix: typo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { promises: fs } = require('fs')
 const main = async () => {
   const path = core.getInput('path')
   const result = await fs.readFile(path, 'utf8')
-  core.setOutput('result', content)
+  core.setOutput('result', result)
 }
 
 main().catch(err => core.setFailed(err.message))
