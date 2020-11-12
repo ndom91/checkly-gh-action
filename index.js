f94a9aa753cc227a4d0729645666fc78dd00da53
ndom91 <yo@ndo.dev>
2020-11-12 17:56:38 +0100

    add: initial queue test

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict'
 
+import { Queue } from 'bullmq'
 const core = require('@actions/core')
 const yamlLint = require('yaml-lint')
 const yaml = require('js-yaml')
 const fetch = require('isomorphic-unfetch')
 const { promises: fs } = require('fs')
-import { Queue } from 'bullmq'
 
 if (!process.env.CHECKLY_API_KEY) {
   core.setFailed('CHECKLY_API_KEY missing!')
@@ -35,27 +35,32 @@ const main = async () => {
       })
         .then(res => res.json())
         .then(async data => {
-					// Check if User can even add Checks
+          // Check if User can even add Checks
           if (data.credits.available > 0) {
             core.info(
               `✅ User ${data.identity.username} has ${data.credits.available} credits remaining`
             )
-						// Create a new Queue
-						const macQueue = new Queue('macQueue', { connection: {
-							host: process.env.REDIS_HOST,
-							port: process.env.REDIS_PORT,
-							password: process.env.REDIS_PW
-						}});
-						if (core.isDebug()) {
-							core.info('✅ Redis Connected')
-						}
+            // Create a new Queue
+            const macQueue = new Queue('macQueue', {
+              connection: {
+                host: process.env.REDIS_HOST,
+                port: process.env.REDIS_PORT,
+                password: process.env.REDIS_PW
+              }
+            })
+            if (core.isDebug()) {
+              core.info('✅ Redis Connected')
+            }
 
-						// Add Json to Queue
-						const checkJson = yaml.safeLoad(checkYaml, 'utf8');
-						const result = await queue.add('checkly:ndo', JSON.stringify(checkJson, null, 2));
+            // Add Json to Queue
+            const checkJson = yaml.safeLoad(checkYaml, 'utf8')
+            const result = await macQueue.add(
+              'checkly:ndo',
+              JSON.stringify(checkJson, null, 2)
+            )
+            core.setOutput('result', result)
           }
         })
-      core.setOutput('result', result)
     })
     .catch(err => {
       // Invalid Yaml
